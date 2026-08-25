3ff5bff7a1dbed276ca92f9784ee64f5cad4dd73
Nathata <nathata.pad@gmail.con>
2026-08-25 20:40:59 +0700

    fix lab5

--- lab5/www/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-export default defineConfig({
-    root: "www",
-    base: "/lab154377/lab5/",
-    build: {
-        outDir: "../lab5",
-        emptyOutDir: true,
-    },
-});
